67689b226012b5356d0a949a6d6e48dddc0ddb9a
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-05-17 16:26:05 +0200

    add: Pokemon class

---
+++ EX01_Pokemon/Model/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX01_Pokemon.Model
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+
+        public string ImageUrl => $"http://static.pokemonpets.com/images/monsters-images-800-\r\n800/{Id}-{Name}.png";
+    }
+}
--- EX01_Pokemon/ViewModel/DetailPageVM.cs
+++ EX01_Pokemon/ViewModel/DetailPageVM.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using EX01_Pokemon.Model;
 
 namespace EX01_Pokemon.ViewModel
 {
     internal class DetailPageVM : ObservableObject
     {
+        public Pokemon Pokemon { get; set; } = new Pokemon()
+        {
+            Id = 136,
+            Name = "Flareon",
+            Type = "Fire",
+            Height = 136,
+            Weight = 9
+        };
     }
 }
