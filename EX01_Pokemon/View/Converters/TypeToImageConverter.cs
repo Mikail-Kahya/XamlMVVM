7ef3bba5d48c20e4f884f6dc0df387344982f2f5
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-05-17 18:04:04 +0200

    feat: type converter

---
+++ EX01_Pokemon/View/Converters/TypeToImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace EX01_Pokemon.View.Converters
+{
+    public class TypeToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string type = value.ToString();
+            BitmapImage result = new BitmapImage(new Uri($"pack://application:,,,/Resources/Bg/{type}.jpg", UriKind.Absolute));
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
