fa486918ad9c0ec3c084901427367e8e1d8ecd2e
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-05-18 12:32:57 +0200

    feat: filtering of types

--- EX01_Pokemon/ViewModel/OverViewPageVM.cs
+++ EX01_Pokemon/ViewModel/OverViewPageVM.cs
@@ -11,14 +11,52 @@ namespace EX01_Pokemon.ViewModel
 {
     public class OverViewPageVM : ObservableObject
     {
-        public List<string> PokemonTypes { get; private set; }
-        public List<Pokemon> Pokemon { get; private set; }
+        private PokemonRepository _repo = new PokemonRepository("Resources/Data/pokemons.json");
+        private string _selectedType = "";
+        private Pokemon _selectedPokemon;
+        private List<string> _pokemonTypes;
+        private List<Pokemon> _pokemon;
+
+        public List<string> PokemonTypes
+        {
+            get => _pokemonTypes;
+            private set
+            {
+                _pokemonTypes = value;
+                OnPropertyChanged(nameof(PokemonTypes));
+            }
+        }
+
+        public List<Pokemon> Pokemon
+        {
+            get => _pokemon;
+            private set
+            {
+                _pokemon = value;
+                OnPropertyChanged(nameof(Pokemon));
+            }
+        }
+
+        public string SelectedType
+        {
+            get => _selectedType;
+            set
+            {
+                _selectedType = value;
+                Pokemon = _repo.GetAllPokemon(_selectedType);
+            }
+        }
+
+        public Pokemon SelectedPokemon
+        {
+            get => _selectedPokemon;
+            set => _selectedPokemon = value;
+        }
 
         public OverViewPageVM()
         {
-            PokemonRepository repo = new PokemonRepository("Resources/Data/pokemons.json");
-            PokemonTypes = repo.Types;
-            Pokemon = repo.Pokemon;
+            PokemonTypes = _repo.Types;
+            Pokemon = _repo.Pokemon;
         }
     }
 }
